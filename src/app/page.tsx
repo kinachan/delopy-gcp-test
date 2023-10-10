0323402c4ccf83c71e48e5ee522271a387362ce8
kinachan <army1102@gmail.com>
2023-10-10 23:10:00 +0900

    GCPBUILDテスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
+        <strong>{process.env.NEXT_PUBLIC_TEST_STR || '失敗した。'}</strong>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
